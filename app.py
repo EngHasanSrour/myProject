b6cd82af25ffd1357e7cbe470cb2c732d3c15a2d
HasanSrour <jojfsh743hv@gmail.com>
2023-11-04 00:27:37 +0200

    Changing in speed in phone and app file

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ import turtle
 t = turtle.Turtle()
 sc = turtle.Screen()
 
+t.speed(0)
 sc.bgcolor('black')
 colors = ['red', 'green', 'blue', 'yellow']
 for i in range(400):
     t.color(colors[i%4])
     t.forward(i*2)
     t.left(144)
-    t.pensize(i*2)
+    t.pensize(i/5 + 1)
