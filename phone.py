dd838ee8bb66b82f99de87a60e25f8df0ac4b18c
HasanSrour <jojfsh743hv@gmail.com>
2023-11-04 11:59:04 +0200

    Changing in phone.py in speed = 0 and range = 100 and creating the mainloop

--- phone.py
+++ phone.py
@@ -5,9 +5,11 @@ sc = turtle.Screen()
 t.speed(0)
 sc.bgcolor('black')
 colors = ['red', 'green', 'blue', 'yellow']
-for i in range(400):
+for i in range(100):
     t.color(colors[i%4])
     t.forward(i*2)
-    t.left(45)
-    t.circle(5, steps=3)
-    t.pensize(i/5 +1)
+    t.left(90)
+    t.forward(i+5)
+    # t.pensize(i/5 +1)
+
+turtle.mainloop()
